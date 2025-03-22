6d4ea07c5963487e2bf4634a996f72ababc2a473
SirDragon94 <giulioguranjaku@gmail.com>
2025-03-22 21:24:51 +0100

    Add About page route

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return "Hello, FocusIA!"
 
+@app.route('/about')
+def about():
+    return "This is the About page for FocusIA!"
+
 if __name__ == "__main__":
     app.run()
